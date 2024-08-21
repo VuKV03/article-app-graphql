8c5c162c9087562707be363d43524620c3740fff
Khuất Văn Vũ <khuatvanvu03@gmail.com>
2024-08-21 16:53:28 +0700

    build: category

--- resolvers.ts
+++ resolvers.ts
@@ -1,4 +1,5 @@
 import Article from "./models/article.model";
+import Category from "./models/category,model";
 
 export const resolvers = {
   Query: {
@@ -17,6 +18,23 @@ export const resolvers = {
       });
       return articles;
     },
+
+    getListCategory: async () => {
+      const categories = await Category.find({
+        deleted: false,
+      });
+      return categories;
+    },
+    getCategory: async (_, args) => {
+      const { id } = args;
+
+      const category = await Category.findOne({
+        _id: id,
+        deleted: false,
+      });
+      return category;
+    },
+
   },
   Mutation: {
     createArticle: async (_, args) => {
@@ -45,16 +63,60 @@ export const resolvers = {
     updateArticle: async (_, args) => {
       const { id, article } = args;
 
-      await Article.updateOne({
-        _id: id,
-        deleted: false
-      }, article);
+      await Article.updateOne(
+        {
+          _id: id,
+          deleted: false,
+        },
+        article
+      );
 
       const record = await Article.findOne({
-        _id: id
-      })
+        _id: id,
+      });
 
       return record;
-    }
+    },
+
+    createCategory: async (_, args) => {
+      const { category } = args;
+
+      const record = new Category(category);
+      await record.save();
+
+      return record;
+    },
+    updateCategory: async (_, args) => {
+      const { id, category } = args;
+
+      await Category.updateOne(
+        {
+          _id: id,
+          deleted: false,
+        },
+        category
+      );
+
+      const record = await Category.findOne({
+        _id: id,
+      });
+
+      return record;
+    },
+    deleteCategory: async (_, args) => {
+      const { id } = args;
+
+      await Category.updateOne(
+        {
+          _id: id,
+        },
+        {
+          deleted: true,
+          deletedAt: new Date(),
+        }
+      );
+
+      return "Đã xóa!";
+    },
   },
 };
--- typeDefs.ts
+++ typeDefs.ts
@@ -2,26 +2,44 @@ import { gql } from "apollo-server-express";
 
 export const typeDefs = gql`
   type Article {
-    id: ID,
-    title: String,
-    avatar: String,
-    description: String,
-  }
-  type Query {
-    hello: String,
-    getListArticle: [Article]
-    getArticle(id: ID): Article
-  }
-
-  input ArticleInput {
-    title: String,
-    avatar: String,
+    id: ID
+    title: String
+    avatar: String
     description: String
   }
 
+  type Category {
+    id: ID
+    title: String
+    avatar: String
+  }
+
+  type Query {
+    getListArticle: [Article]
+    getArticle(id: ID): Article
+
+    getListCategory: [Category]
+    getCategory(id: ID): Category
+  }
+
+  input ArticleInput {
+    title: String
+    avatar: String
+    description: String
+  }
+
+  input CategoryInput {
+    title: String
+    avatar: String
+  }
+
   type Mutation {
-    createArticle(article: ArticleInput): Article,
-    deleteArticle(id: ID): String,
+    createArticle(article: ArticleInput): Article
+    deleteArticle(id: ID): String
     updateArticle(id: ID, article: ArticleInput): Article
+
+    createCategory(category: CategoryInput): Category
+    deleteCategory(id: ID): String
+    updateCategory(id: ID, category: CategoryInput): Category
   }
 `;
