8ef35913ae2f2e3fe087137782dccf9a35634422
Khuất Văn Vũ <khuatvanvu03@gmail.com>
2024-08-21 20:31:24 +0700

    build: typeDefs

--- index.ts
+++ index.ts
@@ -2,8 +2,9 @@ import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
 import * as database from "./config/database";
 import { ApolloServer, gql } from "apollo-server-express";
-import { typeDefs } from "./typeDefs";
+
 import { resolvers } from "./resolvers"
+import { typeDefs } from "./typeDefs/index.typeDefs";
 
 const startServer = async () => {
   dotenv.config();
@@ -15,7 +16,7 @@ const startServer = async () => {
 
   // GraphQL
   const apolloServer = new ApolloServer({
-    typeDefs,
+    typeDefs: typeDefs,
     resolvers,
   });
 
--- typeDefs/article.typeDefs.ts
+++ typeDefs/article.typeDefs.ts
@@ -1,6 +1,6 @@
 import { gql } from "apollo-server-express";
 
-export const typeDefs = gql`
+export const typeDefsArticle = gql`
   type Article {
     id: ID
     title: String
@@ -9,38 +9,21 @@ export const typeDefs = gql`
     category: Category
   }
 
-  type Category {
-    id: ID
-    title: String
-    avatar: String
-  }
-
   type Query {
     getListArticle: [Article]
     getArticle(id: ID): Article
-
-    getListCategory: [Category]
-    getCategory(id: ID): Category
   }
 
   input ArticleInput {
     title: String
     avatar: String
     description: String
-  }
-
-  input CategoryInput {
-    title: String
-    avatar: String
+    categoryId: String
   }
 
   type Mutation {
     createArticle(article: ArticleInput): Article
     deleteArticle(id: ID): String
     updateArticle(id: ID, article: ArticleInput): Article
-
-    createCategory(category: CategoryInput): Category
-    deleteCategory(id: ID): String
-    updateCategory(id: ID, category: CategoryInput): Category
   }
 `;
